bbd9c05b0b9b654de8a4817f779348ebbe45e6c0
implaymo <goncalomoreirasilva@gmail.com>
2025-01-07 10:06:45 +0000

    Update

--- src/main/java/pokedexwebapp/PokedexRestController.java
+++ src/main/java/pokedexwebapp/PokedexRestController.java
@@ -3,12 +3,10 @@ package pokedexwebapp;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest;
 import pokedexwebapp.pokemonApi.PokedexApi;
 import pokedexwebapp.pokemonApi.PokedexJsonParser;
 
